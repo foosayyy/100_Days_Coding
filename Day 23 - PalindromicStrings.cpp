b1602370d326e56dbb1487ebd79acbb9c125cc8d
foosayyy <85921796+foosayyy@users.noreply.github.com>
2021-10-12 23:41:58 +0530

    Create Day 23 - PalindromicStrings.cpp

---
+++ Day 23 - PalindromicStrings.cpp
@@ -0,0 +1,9 @@
+
+#include<bits/stdc++.h>using 
+namespace std;
+int main(){
+string s;
+cin>>s;//comparing string s with the reversed string s 
+if(s==string(s.rbegin(),s.rend())) { 
+   cout<<"YES"<<endl;}else{    
+cout<<"NO"<<endl;}}
